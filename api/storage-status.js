e92d662f30bf637251ecc1b9c3bce3b50a1028e3
onejaeone-alt <onejaeone@gmail.com>
2026-09-04 14:42:06 +0900

    chore: 저장 상태 응답 형식 명시

--- api/storage-status.js
+++ api/storage-status.js
@@ -3,6 +3,7 @@ const { loadStorageStatus } = require("../lib/supabase");
 module.exports = async (_req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "no-store");
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
   try {
     const storage = await loadStorageStatus();
     return res.status(200).json({ ok: true, storage, checked_at: new Date().toISOString() });
